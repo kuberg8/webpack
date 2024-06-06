7f1744ba2299e386e85a705d9ccc09ddde6c40c3
Алексей Демиденко <kuberg8@gmail.com>
2024-06-07 00:44:36 +0500

    fix

--- src/pages/Code.jsx
+++ src/pages/Code.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+import React from "react"
+import {basicSetup, EditorView} from "codemirror"
+import {EditorState} from "@codemirror/state"
+
 
 function Code() {
+  React.useEffect(() => {
+    console.log("React")
+
+    let state = EditorState.create({
+      extensions: [
+        basicSetup,
+      ]
+    })
+    
+    new EditorView({
+      state,
+      parent: document.getElementById("editor")
+    })
+  }, [])
+
   return (
     <div className="py-4 container-md">
       <h1 className="pb-5">Алгоритмы и структуры данных на JavaScript</h1>
+      <hr />
+      <h1 className="pb-5">Задачи</h1>
+      <div id="editor"></div>
     </div>
-  );
+  )
 }
 
-export default React.createElement(Code);
+export default React.createElement(Code)
